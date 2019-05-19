9435a81be6f7afc5b3f8d8bf7ba0934c73f16f44
pkurjanowicz <pkurjanowicz5@gmail.com>
2019-05-19 09:46:51 -0600

    Update caesar.py

--- caesar.py
+++ caesar.py
@@ -1,5 +1,32 @@
-from helpers import alphabet_position, rotate_character
+from helpers import alphabet_position
 
-def encrypt(text, rot):
-    #Add your code here
-    return
+def rotN(message, rot):
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    alphabet = alphabet * 2
+    alphabet_upper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    alphabet_upper = alphabet_upper * 2
+    result = ""
+    for char in message:
+        if char == "," or char == "!" or char == " ":
+            result += char
+        if char.isupper() == True:
+            char_idx= alphabet_upper.find(char)
+            rot_idx = char_idx + rot
+            result = result + alphabet_upper[rot_idx]
+        if char.islower() == True:
+            char_idx= alphabet_position(char)
+            rot_idx = char_idx + rot
+            result = result + alphabet[rot_idx]
+    return result
+        
+
+def main():
+    from sys import argv,exit
+    if argv[1].isdigit() == True:
+        message = input("Type a message:")
+        argv[1] = int(argv[1])
+        print(rotN(message, argv[1]))  
+    else:
+        exit("usage: python caesar.py n, n must be an interger")
+if __name__ == "__main__":
+    main()
